789cd1837132767629a3fca35cf06f2c818b8858
lise <lslvxy@gmail.com>
2017-07-11 17:31:18 +0800

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
 cd ./public
-# git init
+git init
 git add .
 git commit -m "blog update"
-git push --force --quiet "git@git.oschina.net:lslvxy/lslvxy.git" master:master
+git push --force --quiet "git@gitee.com:lslvxy/lslvxy.git" master:master
+git push --force --quiet "git@github.com:lslvxy/lslvxy.github.com.git" master:master
